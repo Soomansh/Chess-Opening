60456f3973b26b4dfb4c768118df9cae7aa6632f
Soomansh <narnesoomansh@gmail.com>
2026-06-05 22:08:33 -0600

    Create extract_puzzles.py

---
+++ extract_puzzles.py
@@ -0,0 +1,13 @@
+import zstandard as zstd
+
+input_file = r"C:\Users\*********\Downloads\lichess_db_puzzle.csv.zst"
+output_file = r"C:\chess_data\lichess_db_puzzle.csv"
+
+dctx = zstd.ZstdDecompressor()
+
+with open(input_file, "rb") as f_in:
+    with open(output_file, "wb") as f_out:
+        dctx.copy_stream(f_in, f_out)
+
+print("EXTRACTION DONE") 
+
